3dd3d6c528d79eb5e83e433edf1d5bb57bd14e5a
aljersulit <aljeromesulit@gmail.com>
2024-10-22 17:28:13 +0800

    feat: add LoginControl's login_user method to call the getUser to retrieve data from database

--- Class/login_control.class.php
+++ Class/login_control.class.php
@@ -2,17 +2,31 @@
 
 declare(strict_types=1);
 
-class LoginControl {
+class LoginControl extends LoginModel {
   private $username_or_email;
-  private $password;
 
   function __construct(string $username_or_email, string $password)
   {
     $this->username_or_email = $this->sanitize_input($username_or_email);
-    $this->password = $this->sanitize_input($password);
   }
 
   // Create login method
+  private function login_user() {
+    $userData = parent::getUser($this->username_or_email, $this->check_if_username());
+    if ($userData) {
+      return $userData;
+    } else {
+      return false;
+    }
+  }
+
+  private function is_password_correct(string $password, string $hashedPWD):bool {
+    if(password_verify($password, $hashedPWD)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   private function check_if_username() {
     if (!filter_var($this->username_or_email, FILTER_VALIDATE_EMAIL)) {
@@ -22,7 +36,7 @@ class LoginControl {
     }
   }
 
-  private function sanitize_input($data)
+  private function sanitize_input(string $data)
   {
     $data = trim($data);
     $data = stripslashes($data);
