0abff90090a5ec81c5ad44aaa7be6209796279df
aljersulit <aljeromesulit@gmail.com>
2024-10-22 17:13:50 +0800

    feat: add LoginModel getUser method
    LoginModel getUser method connects to DB and queries users data id the user exist

--- Class/login_model.class.php
+++ Class/login_model.class.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
-class LoginModel {
-  
+class LoginModel extends Dbhandler {
+  protected function getUser(string $username_or_email, bool $is_username):array|bool {
+    $dbconn = parent::connect();
+
+    if ($is_username) {
+      $query = "SELECT * FROM users WHERE username = :username";
+      $stmt = $dbconn->prepare($query);
+      $stmt->bindParam(":username", $username_or_email);
+    } else {
+      $query = "SELECT * FROM users WHERE email = :email";
+      $stmt = $dbconn->prepare($query);
+      $stmt->bindParam(":email", $username_or_email);
+    }
+
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+  }
 }
