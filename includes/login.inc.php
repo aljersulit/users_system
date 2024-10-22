04e52d1b45c8ca1f15b54a8ce5c00347546f8516
aljersulit <aljeromesulit@gmail.com>
2024-10-22 16:12:33 +0800

    feat: :art: update login.inc.php file to handle submitted data coming from login form

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,20 @@
 <?php
+
+ini_set("display_errors", 1);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $username_or_email = $_POST["username"];
+  $password = $_POST["password"];
+
+  require_once "../Class/Dbhandler.class.php";
+  require_once "../Class/login_model.class.php";
+  require_once "../Class/login_control.class.php";
+
+  require_once "config_session.inc.php";
+
+  header("Location: ../index.php");
+  exit();
+} else {
+  header("Location: ../index.php");
+  die();
+}
