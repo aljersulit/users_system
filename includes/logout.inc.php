34424553e38e2b86c54b315521e7807efeb350f7
aljersulit <aljeromesulit@gmail.com>
2024-10-23 17:02:51 +0800

    feat: add logout button that shows only when the user is loggen in then logs out the user when clicked

---
+++ includes/logout.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  require_once "config_session.inc.php";
+  session_unset();
+  session_destroy();
+  header("Location: ../index.php");
+} else {
+  header("Location: ../index.php");
+  die();
+}
